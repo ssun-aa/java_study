ce8c7a8e940281b360c8689fe70643c3154758be
ssun-aa <ssk6200@daum.net>
2019-02-24 18:42:07 +0900

    JAVA STUDY 과제

---
+++ src/BoxExam.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BoxExam {
+
+	public static Map<String, Integer> makeMap(){
+		Map<String, Integer> product = new HashMap<>();
+		product.put("가위", 2500);
+		product.put("크레파스", 5000);
+		
+		return product;
+	}
+	public static void main(String[] args) {
+		Map<String, Integer> product = new HashMap<>();
+		
+		product = makeMap();
+		Set<String> keys = product.keySet();
+		Iterator<String> iter = keys.iterator();
+		while(iter.hasNext()) {
+			String key = iter.next();
+			System.out.println(key +":"+product.get(key));
+		}
+	}
+}
--- src/Hw5.java
+++ src/Hw5.java
@@ -1,9 +1,32 @@
 
+import java.util.*;
+class Location{
+	
+	public double latitude;
+	public double longitude;
+		
+}
+
 public class Hw5 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Map<String, Location> map = new HashMap<String, Location>();
+		Scanner sc = new Scanner(System.in);
+		Location [] location = new Location[3];
+		for(int i=0;i<3;i++) {
+			String key = sc.next();
+			location[i] = new Location();
+			location[i].latitude = sc.nextDouble();
+			location[i].longitude = sc.nextDouble();
+			
+			map.put(key, location[i]);
+		}
+//		
+//		
+		sc.nextLine();
+		String KEY = sc.nextLine();
+		System.out.println(map.get(KEY).latitude +" "+map.get(KEY).longitude );
+		
+		
 	}
-
 }
