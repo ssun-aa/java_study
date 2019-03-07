eafb5bea03bcac32d556fddbf6a07d7020a713cc
ssun-aa <ssk6200@daum.net>
2019-03-07 15:27:49 +0900

    JAVA STUDY

---
+++ src/Q3_0305.java
@@ -0,0 +1,32 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Q3_0305{
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		
+		
+		cal1.set(2019, 3, 9);
+		
+		long today = System.currentTimeMillis();
+		long then = cal1.getTimeInMillis();
+		
+		long sum = then-today;
+		sum = sum / 1000;
+		sum = sum/60/60/24;
+
+		if(then > today )
+			System.out.println("기준일은 오늘로부터" + sum + "일 지났습니다");
+		else if(then < today)
+			System.out.println("오늘은 기준일로부터" + sum + "일 남았습니다");
+		else
+			System.out.println("기준일 오늘과 같습니다.");
+		
+				
+	}
+}
---
+++ src/Q4_0305.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Q4_0305 {
+
+	public static void main(String[] args) {
+		BufferedReader in = null;
+		PrintWriter pw = null;
+		String word;
+		
+		try {
+			in = new BufferedReader(new FileReader("data.txt"));
+			pw = new PrintWriter(new FileWriter("answer.txt"));
+			ArrayList<String> list = new ArrayList<String>();
+			word = null;
+			System.out.println("<오름차순 정렬 전>");
+			while((word = in.readLine() )!= null) {
+				list.add(word);
+				System.out.println(word);
+			}
+			in.close();
+			Collections.sort(list);
+			Iterator<String> iter = list.iterator();
+			System.out.println("오름차순 정렬 후");
+			while(iter.hasNext()) {
+				word = iter.next();
+				System.out.println(word);
+				pw.println(word);
+			}
+			pw.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
