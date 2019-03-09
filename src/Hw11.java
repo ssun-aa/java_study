6c4184b2b12b1060d06decc04a1f974059073898
ssun-aa <ssk6200@daum.net>
2019-03-09 16:37:06 +0900

    JAVA STUDY 과제

--- src/Hw11.java
+++ src/Hw11.java
@@ -1,4 +1,48 @@
+import java.util.*;
+import java.io.*;
 
 public class Hw11 {
+	public static void main(String[] args) {
+		
+		File file1 = new File("a.jpg");
+		File file2 = new File("b.jpg");
+		
+		long fsize1 = file1.length();//원본 파일의 크기
+		System.out.println("원본 파일의 크기: "+fsize1);
+		
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
 
+		try {
+			fis = new FileInputStream(file1);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			fos = new FileOutputStream(file2);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int input = 0, count = 0;
+		
+		byte[] data = new byte[1024];
+		
+		try {
+			while((input = fis.read(data))!=-1) {
+				fos.write(data, 0, input);
+				count+=input;
+
+				if(count % 10 == 0) {
+					float per = ((float)count/fsize1)*100;
+					System.out.println((int)per +"% 카피 됨.");
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/Hw12.java
@@ -0,0 +1,209 @@
+import java.util.*;
+
+class Customer{
+	String name;
+	int number;
+	
+	public Customer(String name, int number){
+		this.name = name;
+		this.number = number;
+	}
+}
+
+public class Hw12 {
+	static Scanner sc = new Scanner(System.in);
+	static String name;
+	static int number, i, level;
+	
+	public static void reset(Customer []c) {
+		int i;
+		for(i = 0 ; i < 10 ; i++)
+			c[i] = new Customer("---",i+1);//"---"로 초기화
+		
+	}
+	public static void reservation(Customer [] c) {
+		
+		for(i = 0 ; i < 10 ; i++)
+			System.out.print(c[i].name+" ");//예약 내역 출력
+		System.out.println();
+		
+		System.out.print("이름 : ");
+		sc.nextLine();
+		name = sc.nextLine();
+		
+		do {
+			System.out.print("번호 : ");
+			number = sc.nextInt();
+			if(number>=1 && number<=10) {//좌석이 유효한 경우
+				if(!c[number-1].name.equals("---"))//예약자가 있을 경우;실패
+					System.out.println("이미 예약된 자리 입니다. 다시 입력 해 주십시오.");
+				else{
+					c[number-1]= new Customer(name, number);//예약자가 없을 경우;성공
+					break;
+				}
+			}
+			else if(number<1 || number>10)//좌석이 유효하지 않은 경우;실패
+				System.out.println("잘못된 번호 입니다. 다시 입력 해 주십시오.");
+		}while(true);//성공 할 때까지 반복
+				
+	}
+	public static void comfirm(Customer [] c) {
+		for(i = 0 ; i < 10 ; i++)
+			System.out.print(c[i].name+" ");
+		System.out.println();
+	}
+	public static void cancle(Customer [] c) {
+		int flag = 0;
+		
+		for(i = 0 ; i < 10 ; i++)
+			System.out.print(c[i].name+" ");
+		System.out.println();
+		sc.nextLine();
+
+		do {
+			System.out.print("이름 : ");
+			name = sc.nextLine();
+			
+			for(i = 0 ; i < 10 ; i++) {
+				if(name.equals(c[i].name)) {
+					c[i].name = "---";
+					flag = 1;
+				}
+			}
+			if(flag==1) {//일치한 이름이 있을 경우;성공
+				System.out.println("취소에 성공하셨습니다.");
+				break;
+			}
+			else
+				System.out.println("일치하는 이름이 없습니다. 다시 입력해 주십시오.");
+		}while(true);
+	}
+	public static void main(String[] args) {
+		
+		int scount = 0, acount = 0, bcount = 0;
+		Customer [] s = new Customer[10];
+		Customer [] a = new Customer[10];
+		Customer [] b = new Customer[10];
+		
+		reset(s);
+		reset(a);
+		reset(b);
+		
+		while(true) {
+			
+			System.out.print("예약(1), 조회(2), 취소(3), 끝내기(4) >>");
+			int n = sc.nextInt();
+			switch (n) {
+			case 1:
+				do {
+				System.out.print("좌석 구분 S(1), A(2), B(3) >>");
+				level = sc.nextInt();
+				
+				if(level==1) {
+					if(scount==10) {
+						System.out.println("예약할 남은 좌석이 없습니다");
+					}
+					else {
+						System.out.print("S>>");
+						reservation(s);
+						scount++;
+						break;
+					}
+				}
+				else if(level==2) {
+					if(acount==10) {
+						System.out.println("예약할 남은 좌석이 없습니다");
+					}
+					else {
+						System.out.print("A>>");
+						reservation(a);
+						acount++;
+						break;
+					}
+				}
+				else if(level==3) {
+					if(bcount==10) {
+						System.out.println("예약할 남은 좌석이 없습니다");
+					}
+					else {
+						System.out.print("B>>");
+						reservation(b);
+						bcount++;
+						break;
+					}
+				}
+				else
+					System.out.println("잘못된 좌석입니다. 다시 입력 해 주십시오");
+				}while(true);
+				
+				break;
+				
+			case 2:
+				System.out.print("S>>");
+				comfirm(s);
+				
+				System.out.print("A>>");
+				comfirm(a);
+				
+				System.out.print("B>>");
+				comfirm(b);
+				
+				System.out.println("<<<조회를 완료하였습니다.>>>");
+				
+				break;
+				
+			case 3:
+				do {
+				System.out.print("좌석 구분 S(1), A(2), B(3) >>");
+				level = sc.nextInt();
+				if(level == 1) {
+					if(scount==0) {
+						System.out.println("취소할 좌석이 없습니다");
+						break;
+					}
+					System.out.print("S>>");
+					cancle(s);
+					scount--;
+					break;
+				}
+				else if(level == 2) {
+					if(acount==0) {
+						System.out.println("취소할 좌석이 없습니다");
+						break;
+					}
+					System.out.print("A>>");
+					cancle(a);
+					acount--;
+					break;
+				}
+				else if(level == 3) {
+					if(bcount==0) {
+						System.out.println("취소할 좌석이 없습니다");
+						break;
+					}
+					System.out.print("B>>");
+					cancle(b);
+					bcount--;
+					break;
+				}
+				else
+					System.out.println("잘못된 좌석입니다. 다시 입력 해 주십시오");
+				}while(true);
+			
+				break;
+				
+			case 4:
+				break;
+				
+			default:
+				System.out.println("없는 메뉴입니다. 다시 입력 해 주십시오.");
+				break;
+			}
+			
+			if(n == 4)
+				break;
+			
+		
+		}
+	}
+}
